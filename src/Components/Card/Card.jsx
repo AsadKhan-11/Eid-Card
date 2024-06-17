bbf0873174386b9e3b51cd83edb1fa88e56a7935
Asad Khan <mrasad10khan@gmail.com>
2024-06-17 16:47:18 +0500

    Creating card

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Card.css";
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-top"></div>
+    </div>
+  );
+}
+
+export default Card;
