9ce130ffe5ee44a6d3891b021a3d3226f0f0ca1b
Asad Khan <mrasad10khan@gmail.com>
2024-06-18 10:25:41 +0500

    Opening the card

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 function Card() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openCard = () => {
+    setIsOpen(true);
+  };
+
   return (
     <div className="card">
-      <div className="card-top">
-        <button className="btn-click">Click here</button>
-      </div>
+      <div
+        className="card-top"
+        style={{
+          borderTopWidth: isOpen ? "0px" : "150px",
+          borderBottomWidth: isOpen ? "150px" : "0px",
+          top: isOpen ? "-150px" : "0px",
+        }}
+      ></div>
+      <button className="btn-click" onClick={openCard}>
+        Click here
+      </button>
     </div>
   );
 }
