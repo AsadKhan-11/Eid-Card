6b5d1e07f97e13579fd1f4882a3d0f0ea0fcfb89
Asad Khan <mrasad10khan@gmail.com>
2024-06-18 18:18:34 +0500

    Adding txt and animation

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Card.css";
+import picture from "../../Pic/picture.jpg";
 function Card() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -8,19 +9,42 @@ function Card() {
   };
 
   return (
-    <div className="card">
-      <div
-        className="card-top"
-        style={{
-          borderTopWidth: isOpen ? "0px" : "150px",
-          borderBottomWidth: isOpen ? "150px" : "0px",
-          top: isOpen ? "-150px" : "0px",
-        }}
-      ></div>
-      <button className="btn-click" onClick={openCard}>
-        Click here
-      </button>
-    </div>
+    <>
+      {" "}
+      <div className="card" style={{ marginTop: isOpen ? "200px" : "0px" }}>
+        <div
+          className="card-top"
+          style={{
+            borderTopWidth: isOpen ? "0px" : "150px",
+            borderBottomWidth: isOpen ? "150px" : "0px",
+            top: isOpen ? "-150px" : "0px",
+          }}
+        ></div>
+        <button
+          className="btn-click"
+          onClick={openCard}
+          style={{ opacity: isOpen ? "0" : "1" }}
+        >
+          Click here
+        </button>
+        <img
+          className="bakra-pic"
+          src={picture}
+          style={{
+            opacity: isOpen ? "1" : "0",
+            top: isOpen ? "-200px" : "0",
+          }}
+        />
+        <h1
+          className="eid-txt"
+          style={{
+            opacity: isOpen ? "1" : "0",
+          }}
+        >
+          Eid Mubarak
+        </h1>
+      </div>
+    </>
   );
 }
 
